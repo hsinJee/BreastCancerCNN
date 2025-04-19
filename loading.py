4a4a6b0c4470c7296fe6d241035fbf3c7ccfd1e2
hsinJee <125839853+hsinJee@users.noreply.github.com>
2025-04-20 00:50:55 +0100

    Included revised conv, dense and new flatten layers
    cnn from scratch complete overhaul
    included a new update to loading as well

--- loading.py
+++ loading.py
@@ -22,6 +22,32 @@ def load_mnist():
         'test_labels': test_labels
     }
 
+def load_mnist_new():
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+    # Reshape to (N, 28, 28, 1)
+    X_train = np.expand_dims(X_train, axis=-1)
+    X_test = np.expand_dims(X_test, axis=-1)
+
+    # Permute and split
+    indices = np.random.permutation(X_train.shape[0])
+    training_idx, validation_idx = indices[:55000], indices[55000:]
+
+    train_images = X_train[training_idx]
+    train_labels = y_train[training_idx]
+    validation_images = X_train[validation_idx]
+    validation_labels = y_train[validation_idx]  
+
+    return {
+        'train_images': train_images,
+        'train_labels': train_labels,
+        'validation_images': validation_images,
+        'validation_labels': validation_labels,
+        'test_images': X_test,
+        'test_labels': y_test
+    }
+
+
 def minmax_normalize(x):
     min_val = np.min(x)
     max_val = np.max(x)
