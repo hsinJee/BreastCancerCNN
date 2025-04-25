8122f889c95850e6baea8eaae9ac7457d3848fd3
hsinJee <125839853+hsinJee@users.noreply.github.com>
2025-04-25 23:49:02 +0100

    sgd > adam dense

--- cnnModelNumpy/dense_new.py
+++ cnnModelNumpy/dense_new.py
@@ -3,13 +3,18 @@ import numpy as np
 class Dense:
     def __init__(self, name, input_size, output_size, useSoftmax=True):
         self.name = name
-
-        # Xavier initialization
         limit = np.sqrt(6 / (input_size + output_size))
         self.weights = np.random.uniform(-limit, limit, (input_size, output_size))
         self.biases = np.zeros(output_size)
         self.last_input = None
         self.useSoftmax = useSoftmax
+
+        # Adam specific variables
+        self.m_w = np.zeros_like(self.weights)
+        self.v_w = np.zeros_like(self.weights)
+        self.m_b = np.zeros_like(self.biases)
+        self.v_b = np.zeros_like(self.biases)
+        self.t = 0
     
     def softmax(self, x):
         exp = np.exp(x - np.max(x, axis=1, keepdims=True))
@@ -26,17 +31,28 @@ class Dense:
 
         return output
     
-    def backward(self, din, learning_rate):
+    def backward(self, din, learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8):
         batch_size = self.last_input.shape[0]
-
         dw = np.dot(self.last_input.T, din) / batch_size
         db = np.sum(din, axis=0) / batch_size
 
-        self.weights -= learning_rate * dw
-        self.biases -= learning_rate * db
+        self.t += 1
+
+        # Update weights using Adam
+        self.m_w = beta1 * self.m_w + (1 - beta1) * dw
+        self.v_w = beta2 * self.v_w + (1 - beta2) * (dw ** 2)
+        m_w_hat = self.m_w / (1 - beta1 ** self.t)
+        v_w_hat = self.v_w / (1 - beta2 ** self.t)
+        self.weights -= learning_rate * m_w_hat / (np.sqrt(v_w_hat) + epsilon)
+
+        # Update biases using Adam
+        self.m_b = beta1 * self.m_b + (1 - beta1) * db
+        self.v_b = beta2 * self.v_b + (1 - beta2) * (db ** 2)
+        m_b_hat = self.m_b / (1 - beta1 ** self.t)
+        v_b_hat = self.v_b / (1 - beta2 ** self.t)
+        self.biases -= learning_rate * m_b_hat / (np.sqrt(v_b_hat) + epsilon)
 
         dout = np.dot(din, self.weights.T)
-
         return dout
     
     def get_weights(self):
